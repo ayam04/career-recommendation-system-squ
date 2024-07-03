3532ab94fbc6808f80383b9d33a7221e8afc0f9b
ayam04 <ayamullahkhan04@gmail.com>
2024-07-03 20:06:25 +0400

    dataset and retrieval update

---
+++ utils.py
@@ -0,0 +1,28 @@
+import json
+import random
+
+def get_p_questions():
+    with open('p-questions.json', 'r') as file:
+        questions = json.load(file)
+
+    random_questions = random.sample(questions, 10)
+    questions_answers_dict = {question: "" for question in random_questions}
+    return questions_answers_dict
+
+def get_i_questions():
+    with open('i-questions.json', 'r') as file:
+        questions = json.load(file)
+    random_questions = random.sample(questions, 10)
+    questions_answers_dict = {question: "" for question in random_questions}
+    return questions_answers_dict
+
+def get_g_questions():
+    with open('g-questions.json', 'r') as file:
+        questions = json.load(file)
+    random_questions = random.sample(questions, 10)
+    questions_answers_dict = {question: "" for question in random_questions}
+    return questions_answers_dict
+
+print(get_p_questions())
+print(get_i_questions())
+print(get_g_questions())
